b9ca43c159e9d00cae8c0435e674ecd2033f1727
Guti200 <130452905+Guti200@users.noreply.github.com>
2023-05-16 12:04:13 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,77 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    tool = 0
 })
+input.onGesture(Gesture.Shake, function () {
+    tool = randint(0, 2)
+    if (tool == 0) {
+        radio.sendString("papel")
+        basic.showIcon(IconNames.Square)
+    } else if (tool == 1) {
+        radio.sendString("tijeras")
+        basic.showIcon(IconNames.Scissors)
+    } else {
+        radio.sendString("piedras")
+        basic.showIcon(IconNames.SmallSquare)
+        if (piedra == piedra) {
+            basic.pause(200)
+            basic.showString("empate")
+        }
+        if (papel == piedra) {
+            basic.pause(200)
+            basic.showString("has ganado")
+        }
+        if (tijera == papel) {
+            basic.pause(200)
+            basic.showString("has ganado")
+        }
+        if (tijera == piedra) {
+            basic.pause(200)
+            basic.showString("has perdido")
+        }
+        if (piedra == tijera) {
+            basic.pause(200)
+            basic.showString("has ganado")
+        }
+    }
+})
+radio.onReceivedString(function (receivedString) {
+    tool = randint(0, 2)
+    if (tool == 0) {
+        radio.sendString("papel")
+        basic.showIcon(IconNames.Square)
+    } else if (tool == 1) {
+        radio.sendString("tijeras")
+        basic.showIcon(IconNames.Scissors)
+    } else {
+        radio.sendString("piedras")
+        basic.showIcon(IconNames.SmallSquare)
+        if (piedra == piedra) {
+            basic.pause(200)
+            basic.showString("empate")
+        }
+        if (papel == piedra) {
+            basic.pause(200)
+            basic.showString("has ganado")
+        }
+        if (tijera == papel) {
+            basic.pause(200)
+            basic.showString("has ganado")
+        }
+        if (tijera == piedra) {
+            basic.pause(200)
+            basic.showString("has perdido")
+        }
+        if (piedra == tijera) {
+            basic.pause(200)
+            basic.showString("has ganado")
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    tool = 1
+})
+let tijera = 0
+let papel = 0
+let piedra = 0
+let tool = 0
+radio.setGroup(69)
